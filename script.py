e108bd11c39f7355be9f1c830d8989115e812be7
Sad <nguyentrungson217@gmail.com>
2023-06-02 23:14:43 +0700

    Added interest finder logic

--- script.py
+++ script.py
@@ -26,7 +26,7 @@ def get_traveler_location(traveler: str):
 attractions = [list() for time in range(5)]  # [[], [], [], [], []]
 
 
-def add_attraction(destination: str, attraction: str):
+def add_attraction(destination: str, attraction: list):
     destination_index = get_destination_index(destination)
     # If destination exist -> a list in attractions
     attractions[destination_index].append(attraction)
@@ -45,4 +45,23 @@ add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
 add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
-print(attractions)  # [[], [], [['Venice Beach', ['beach']]], [], []]
+# print(attractions)  # [[], [], [['Venice Beach', ['beach']]], [], []]
+
+
+def find_attractions(destination: str, interests: list):
+    destination_index = get_destination_index(destination)
+    attractions_in_city = attractions[destination_index]
+    attractions_with_interest = []
+
+    for possible_attraction in attractions_in_city:
+        attraction_tags = possible_attraction[1]
+
+        for interest in interests:
+            if interest in attraction_tags:
+                attractions_with_interest.append(possible_attraction[0])
+
+    return attractions_with_interest
+
+
+la_arts = find_attractions("Los Angeles, USA", ["art"])
+print(la_arts)
